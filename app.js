477083cafe0ce80635eda18d585feeaa4321c1da
Aggunkr <aggunahmet2763@gmail.com>
2025-05-02 20:50:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname,"uploads")));
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Statik dosyaları servis et
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(()=>console.log("✅ MongoDB Bağlandı"))
