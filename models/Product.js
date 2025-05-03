e696ef45b4d19f15323891be07418e6efc7a7d02
Aggunkr <aggunahmet2763@gmail.com>
2025-05-03 10:59:28 +0300

    Şık Tailwind temayı ekledim ve son remote değişikliklerle entegre ettim

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,29 @@
-// models/Product.js
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name:       { type: String, required: true },
-  price:      { type: Number, required: true },
-  description:{ type: String },
-  category:   { type: String },
-  imageUrl:   { type: String }
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  stock: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: String,
+  }
 }, { timestamps: true });
 
-module.exports = mongoose.model("Product", productSchema);
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
