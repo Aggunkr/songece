57e666cd7a4b4bd95e6757f14d6580db4dc81596
Aggunkr <aggunahmet2763@gmail.com>
2025-05-03 15:28:59 +0300

    Yeni proje yüklendi

--- routes/favoriteRoutes.js
+++ routes/favoriteRoutes.js
@@ -1,11 +1,28 @@
-const express = require("express");
-const router = express.Router();
-const { addToFavorites, getFavorites, removeFavorite } = require("../controllers/favoriteController");
-// Burada bütün auth middleware nesnesini almak yerine yalnızca protect fonksiyonunu alıyoruz:
-const { protect } = require("../middleware/authMiddleware");
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
 
-router.post("/favorites", protect, addToFavorites);
-router.get("/favorites", protect, getFavorites);
-router.delete("/favorites/:productId", protect, removeFavorite);
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
 
-module.exports = router;
+PS C:\Masaüstü\AggunECommerceww> npm start
+
+> aggunecommerce@1.0.0 start
+> node app.js
+
+C:\Masaüstü\AggunECommerceww\node_modules\express\lib\router\route.js:216
+        throw new Error(msg);
+        ^
+
+Error: Route.post() requires a callback function but got a [object Undefined]
+    at Route.<computed> [as post] (C:\Masaüstü\AggunECommerceww\node_modules\express\lib\router\route.js:216:15)
+    at proto.<computed> [as post] (C:\Masaüstü\AggunECommerceww\node_modules\express\lib\router\index.js:521:19)
+    at Object.<anonymous> (C:\Masaüstü\AggunECommerceww\routes\favoriteRoutes.js:7:8)
+    at Module._compile (node:internal/modules/cjs/loader:1554:14)
+    at Object..js (node:internal/modules/cjs/loader:1706:10)
+    at Module.load (node:internal/modules/cjs/loader:1289:32)
+    at Function._load (node:internal/modules/cjs/loader:1108:12)
+    at TracingChannel.traceSync (node:diagnostics_channel:322:14)
+    at wrapModuleLoad (node:internal/modules/cjs/loader:220:24)
+    at Module.require (node:internal/modules/cjs/loader:1311:12)
+
+Node.js v22.14.0
+PS C:\Masaüstü\AggunECommerceww>
